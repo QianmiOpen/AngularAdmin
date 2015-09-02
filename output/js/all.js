72891f5ecf8a022c5d4aef9b0bfa50ab35b45854
binlaniua <binlaniua@gmail.com>
2015-09-02 17:38:23 +0800

    [重构] spinner的model设置的值，也受min和max影响

--- output/js/all.js
+++ output/js/all.js
@@ -2571,23 +2571,34 @@ angular.module('admin.component')
                     super$0.prototype.initEvents.call(this);
                     this.element.on('mousedown', '.spinner-up', function()  {return this$0._changeValue(true)});
                     this.element.on('mousedown', '.spinner-down', function()  {return this$0._changeValue(false)});
+                    this.scope.$watch('model', function(n)  {
+                        var cn = this$0._checkValue(n);
+                        if (cn != n) {
+                            this$0.scope.model = cn;
+                        }
+                    });
                 };
 
                 proto$0._changeValue = function(isAdd) {
                     var step = (this.attrs.step || 1) * 1,
-                        min = (this.attrs.min || 0) * 1,
-                        max = (this.attrs.max || Number.MAX_VALUE) * 1,
                         val = this.val();
                     val = val !== undefined ? parseInt(val) : this.attrs.value;
                     val = val + (step * ( isAdd ? 1 : -1));
-                    if (val > max) {
-                        val = max;
-                    }
-                    if (val < min) {
-                        val = min;
-                    }
+                    val = this._changeValue(val);
                     this.scope.model = val;
                     this.scope.change({val: val});
+                };
+
+                proto$0._checkValue = function(value) {
+                    var min = (this.attrs.min || 0) * 1,
+                        max = (this.attrs.max || Number.MAX_VALUE) * 1;
+                    if (value > max) {
+                        value = max;
+                    }
+                    if (value < min) {
+                        value = min;
+                    }
+                    return value;
                 };
             MIXIN$0(UISpinnerControl.prototype,proto$0);proto$0=void 0;return UISpinnerControl;})(UIFormControl);
             return UISpinnerControl;
--- src/components/form/service/spinner.factory.js
+++ src/components/form/service/spinner.factory.js
@@ -23,23 +23,34 @@
                     super.initEvents();
                     this.element.on('mousedown', '.spinner-up', () => this._changeValue(true));
                     this.element.on('mousedown', '.spinner-down', () => this._changeValue(false));
+                    this.scope.$watch('model', (n) => {
+                        let cn = this._checkValue(n);
+                        if (cn != n) {
+                            this.scope.model = cn;
+                        }
+                    });
                 }
 
                 _changeValue(isAdd) {
                     let step = (this.attrs.step || 1) * 1,
-                        min = (this.attrs.min || 0) * 1,
-                        max = (this.attrs.max || Number.MAX_VALUE) * 1,
                         val = this.val();
                     val = val !== undefined ? parseInt(val) : this.attrs.value;
                     val = val + (step * ( isAdd ? 1 : -1));
-                    if (val > max) {
-                        val = max;
-                    }
-                    if (val < min) {
-                        val = min;
-                    }
+                    val = this._changeValue(val);
                     this.scope.model = val;
                     this.scope.change({val: val});
+                }
+
+                _checkValue(value) {
+                    let min = (this.attrs.min || 0) * 1,
+                        max = (this.attrs.max || Number.MAX_VALUE) * 1;
+                    if (value > max) {
+                        value = max;
+                    }
+                    if (value < min) {
+                        value = min;
+                    }
+                    return value;
                 }
             }
             return UISpinnerControl;
