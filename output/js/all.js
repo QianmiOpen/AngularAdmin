b0ff364488c07175e1e5cb0f297d0b2d6b7d566c
binlanlua <binlaniua@gmail.com>
2015-09-04 00:10:39 +0800

    [重构] 新增ui-form-upload, 采用七牛上传

--- output/js/all.js
+++ output/js/all.js
@@ -2784,6 +2784,121 @@ angular.module('admin.component')
 //
 //
 //-----------------------------------------------------------------------------------------------
+(function () {
+
+    angular.module('admin.component')
+        .provider('UIUploadControl', function()  {
+            var domain, tokenUrl, maxSize,
+                result = {
+                    setDomain: function(_domain) {
+                        domain = _domain
+                    },
+
+                    setTokenUrl: function(_tokenUrl) {
+                        tokenUrl = _tokenUrl;
+                    },
+
+                    setMaxSize: function(_maxSize) {
+                        maxSize = _maxSize;
+                    },
+
+                    $get: function() {
+                        var UIUploadControl = (function(super$0){var PRS$0 = (function(o,t){o["__proto__"]={"a":t};return o["a"]===t})({},{});var DP$0 = Object.defineProperty;var GOPD$0 = Object.getOwnPropertyDescriptor;var MIXIN$0 = function(t,s){for(var p in s){if(s.hasOwnProperty(p)){DP$0(t,p,GOPD$0(s,p));}}return t};var SP$0 = Object.setPrototypeOf||function(o,p){if(PRS$0){o["__proto__"]=p;}else {DP$0(o,"__proto__",{"value":p,"configurable":true,"enumerable":false,"writable":true});}return o};var OC$0 = Object.create;if(!PRS$0)MIXIN$0(UIUploadControl, super$0);var proto$0={};
+
+                            function UIUploadControl(s, e, a) {
+                                this.className = 'Upload';
+                                super$0.call(this, s, e, a);
+                            }if(super$0!==null)SP$0(UIUploadControl,super$0);UIUploadControl.prototype = OC$0(super$0!==null?super$0.prototype:null,{"constructor":{"value":UIUploadControl,"configurable":true,"writable":true}});DP$0(UIUploadControl,"prototype",{"configurable":false,"enumerable":false,"writable":false});
+
+                            proto$0.init = function() {
+                                super$0.prototype.init.call(this);
+                                this.$updateButton = this.element.find('button:eq(0)');
+                                this.$removeButton = this.element.find('button:eq(1)');
+                                this.$container = this.element.find('.ui-form-upload');
+                                this.uploadButtonId = 'uiFormUploadButton' + new Date().getTime();
+                                this.uploadContrainerId = 'uiFormUploadContainer' + new Date().getTime();
+                                this.$updateButton.attr('id', this.uploadButtonId);
+                                this.$container.attr('id', this.$container);
+                            };
+
+                            proto$0.render = function() {
+                                super$0.prototype.render.call(this);
+                                Qiniu.uploader({
+                                    runtimes: 'html5,flash,html4',    //上传模式,依次退化
+                                    browse_button: this.uploadButtonId,       //上传选择的点选按钮，**必需**
+                                    uptoken_url: tokenUrl,
+                                    domain: domain,
+                                    unique_names: true,
+                                    container: this.uploadContrainerId,           //上传区域DOM ID，默认是browser_button的父元素，
+                                    max_file_size: maxSize,           //最大文件体积限制
+                                    flash_swf_url: '/assets/js/plupload/js/Moxie.swf',  //引入flash,相对路径
+                                    max_retries: 3,                   //上传失败最大重试次数
+                                    dragdrop: true,                   //开启可拖曳上传
+                                    drop_element: this.uploadContrainerId,        //拖曳上传区域元素的ID，拖曳文件或文件夹后可触发上传
+                                    chunk_size: '1mb',                //分块上传时，每片的体积
+                                    auto_start: true,                 //选择文件后自动上传，若关闭需要自己绑定事件触发上传
+                                    init: {
+                                        'FilesAdded': function (up, files) {
+                                            plupload.each(files, function (file) {
+                                                // 文件添加进队列后,处理相关的事情
+                                            });
+                                        },
+                                        'BeforeUpload': function (up, file) {
+                                            var a = 1;
+                                            // 每个文件上传前,处理相关的事情
+                                        },
+                                        'UploadProgress': function (up, file) {
+                                            var a = 1;
+                                            // 每个文件上传时,处理相关的事情
+                                        },
+                                        'FileUploaded': function (up, file, info) {
+                                            info = JSON.parse(info);
+                                            info.name = file.name;
+                                            info.url = domain + '/' + info.key;
+                                            cb && cb(info);
+                                            // 每个文件上传成功后,处理相关的事情
+                                            // 其中 info 是文件上传成功后，服务端返回的json，形式如
+                                            // {
+                                            //    "hash": "Fh8xVqod2MQ1mocfI4S4KpRL6D98",
+                                            //    "key": "gogopher.jpg"
+                                            //  }
+                                            // 参考http://developer.qiniu.com/docs/v6/api/overview/up/response/simple-response.html
+                                            // var domain = up.getOption('domain');
+                                            // var res = parseJSON(info);
+                                            // var sourceLink = domain + res.key; 获取上传成功后的文件的Url
+                                        },
+                                        'Error': function (up, err, errTip) {
+                                            var a = 1;
+                                            //上传出错时,处理相关的事情
+                                        },
+                                        'UploadComplete': function () {
+                                            var a = 1;
+                                            //队列文件处理完毕后,处理相关的事情
+                                        },
+                                        'Key': function (up, file) {
+                                            // 若想在前端对每个文件的key进行个性化处理，可以配置该函数
+                                            // 该配置必须要在 unique_names: false , save_key: false 时才生效
+                                            var key = "";
+                                            // do something with key here
+                                            return key
+                                        }
+                                    }
+                                });
+                            };
+                        MIXIN$0(UIUploadControl.prototype,proto$0);proto$0=void 0;return UIUploadControl;})(UIFormControl);
+                        return UIUploadControl;
+                    }
+                };
+            return result;
+        });
+})();
+//-----------------------------------------------------------------------------------------------
+//
+//
+//  针对input的封装
+//
+//
+//-----------------------------------------------------------------------------------------------
 angular.module('admin.component')
     .directive('uiFormEditor', function (UIEditorControl) {
         return {
@@ -3178,6 +3293,54 @@ angular.module('admin.component')
         };
     });
 
+//-----------------------------------------------------------------------------------------------
+//
+//
+//  针对input的封装
+//
+//
+//-----------------------------------------------------------------------------------------------
+angular.module('admin.component')
+    .directive('uiFormUpload', function (UIInputControl) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                lcol: '@',
+                rcol: '@',
+                label: '@',
+                css: '@',
+                name: '@',
+                model: '=',
+                change: '&',
+                help: '@'
+            },
+            link: function (scope, element, attrs) {
+                new UIInputControl(scope, element, attrs);
+            },
+            template: ("\
+\n                <div class=\"form-group\">\
+\n                   <label class=\"col-md-{{lcol || DefaultCol.l}} control-label\">{{label}}</label>\
+\n                   <div class=\"col-md-{{rcol || DefaultCol.r}} ui-form-upload\">\
+\n                       <div>\
+\n                           <img class=\"{{css}}\"/>\
+\n                       </div>\
+\n                       <div class=\"btn-group\">\
+\n                           <button type=\"button\" class=\"btn blue start\">\
+\n                               <i class=\"fa fa-upload\"></i>\
+\n                               <span>选择文件</span>\
+\n                           </button>\
+\n                           <button type=\"button\" class=\"btn red start\">\
+\n                               <i class=\"fa fa-upload\"></i>\
+\n                               <span>删除文件</span>\
+\n                           </button>\
+\n                       </div>\
+\n                       <span ng-if=\"help\" class=\"help-block\">{{help}}</span>\
+\n                   </div>\
+\n               </div>'\
+\n            ")
+        };
+    });
 //-----------------------------------------------------------------------------------------------
 //
 //
@@ -3662,7 +3825,7 @@ angular.module('admin.component')
 //-----------------------------------------------------------------------------------------------
 (function () {
     angular.module('admin', ['admin.service', 'admin.filter', 'admin.component'])
-        .config(function(AjaxProvider, MessageProvider, UIEditorControlProvider)  {
+        .config(function(AjaxProvider, MessageProvider, UIEditorControlProvider, UIUploadControlProvider)  {
             var baseJsUrl = 'http://localhost:63342/AngularAdmin/output/assets/js/';
 
             //
@@ -3680,5 +3843,12 @@ angular.module('admin.component')
             // 百度编辑器的库地址
             //
             UIEditorControlProvider.setUrl((("" + baseJsUrl) + "/ueditor/ueditor.config.js"), (("" + baseJsUrl) + "/ueditor/ueditor.all.js"));
+
+            //
+            // 上传空间的配置
+            //
+            UIUploadControlProvider.setDomain('七牛域名');
+            UIUploadControlProvider.setTokenUrl('七牛每次上传会调用这个URL, 返回算好的token, 然后才能上传');
+            UIUploadControlProvider.setMaxSize('1mb');
         });
 })();
--- src/admin.js
+++ src/admin.js
@@ -7,7 +7,7 @@
 //-----------------------------------------------------------------------------------------------
 (function () {
     angular.module('admin', ['admin.service', 'admin.filter', 'admin.component'])
-        .config((AjaxProvider, MessageProvider, UIEditorControlProvider) => {
+        .config((AjaxProvider, MessageProvider, UIEditorControlProvider, UIUploadControlProvider) => {
             let baseJsUrl = 'http://localhost:63342/AngularAdmin/output/assets/js/';
 
             //
@@ -25,5 +25,12 @@
             // 百度编辑器的库地址
             //
             UIEditorControlProvider.setUrl(`${baseJsUrl}/ueditor/ueditor.config.js`, `${baseJsUrl}/ueditor/ueditor.all.js`);
+
+            //
+            // 上传空间的配置
+            //
+            UIUploadControlProvider.setDomain('七牛域名');
+            UIUploadControlProvider.setTokenUrl('七牛每次上传会调用这个URL, 返回算好的token, 然后才能上传');
+            UIUploadControlProvider.setMaxSize('1mb');
         });
 })();
