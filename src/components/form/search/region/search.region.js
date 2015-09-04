39bfd733a927f9a57746a5708cb6c09d35a8daaf
binlanlua <binlaniua@gmail.com>
2015-09-04 23:21:37 +0800

    [重构] ui-search-region已上

--- src/components/form/search/region/search.region.js
+++ src/components/form/search/region/search.region.js
@@ -6,13 +6,33 @@
 //
 //-----------------------------------------------------------------------------------------------
 angular.module('admin.component')
-    .directive('uiSearchRegion', function (uiRegionService, componentHelper) {
+    .directive('uiSearchRegion', function (UIRegionControl) {
         return {
             restrict: 'E',
             replace: true,
-            link: uiRegionService,
-            template: function (element, attrs) {
-                return componentHelper.getTemplate('tpl.searchform.region', attrs);
-            }
+            scope: {
+                model: '=',
+                change: '&',
+                label: '@',
+                name: '@',
+                mode: '@'
+            },
+            link: (s, e, a) => {
+                if (a.mode === undefined || a.mode == 'a') { //区域查询不支持详细地址
+                    a.mode = 's';
+                }
+                new UIRegionControl(s, e, a);
+            },
+            template: `
+                <div class="input-inline search-item">
+                    <div class="input-group ui-search-region">
+                        <div ng-if="label" class="input-group-addon">{{label}}:</div>
+                        <input type="hidden" name="{{name}}"/>
+                        <input type="text" class="input-small form-control input-inline" name="province"/>
+                        <input type="text" class="input-small form-control input-inline" style="left:-1px" name="city"/>
+                        <input type="text" class="input-small form-control input-inline" style="left:-2px" name="area"/>
+                    </div>
+                </div>
+            `
         };
     });
