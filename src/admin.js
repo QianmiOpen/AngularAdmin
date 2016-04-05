752c3a66a0b4b0573f092f8c000efe1558b10cb0
binlaniua <binlaniua@gmail.com>
2016-04-05 18:08:40 +0800

    ajax 支持跨域, 支持setHost默认域名

--- src/admin.js
+++ src/admin.js
@@ -6,45 +6,46 @@
 //
 //-----------------------------------------------------------------------------------------------
 (function () {
-    angular.module('admin', ['admin.service', 'admin.filter', 'admin.component', 'ui.router'])
-        .constant('AdminCDN', 'http://7xllk7.com1.z0.glb.clouddn.com')
-        .config((AdminCDN, AjaxProvider, MessageProvider, UIEditorControlProvider, UIUploadControlProvider, UITableControlProvider, UITreeControlProvider) => {
+  angular.module('admin', ['admin.service', 'admin.filter', 'admin.component', 'ui.router'])
+         .constant('AdminCDN', 'http://7xllk7.com1.z0.glb.clouddn.com')
+         .config((AdminCDN, AjaxProvider, MessageProvider, UIEditorControlProvider, UIUploadControlProvider, UITableControlProvider, UITreeControlProvider) => {
 
-            //
-            // ajax 默认返回处理
-            //
-            AjaxProvider.setSuccessHandler((result) => result.type == 1 ? result.data : null);
-            AjaxProvider.setFailHandler((result) => result.type != 1 ? result.data : null);
+           //
+           // ajax 默认返回处理
+           //
+           AjaxProvider.setHost('');
+           AjaxProvider.setSuccessHandler((result) => result.type == 1 ? result.data : null);
+           AjaxProvider.setFailHandler((result) => result.type != 1 ? result.data : null);
 
-            //
-            // 通知位置
-            //
-            MessageProvider.setPosition('bottom', 'right');
+           //
+           // 通知位置
+           //
+           MessageProvider.setPosition('bottom', 'right');
 
-            //
-            // 百度编辑器的库地址
-            //
-            UIEditorControlProvider.setUrl(`${AdminCDN}/assets/js/ueditor/ueditor.config.js`, `${AdminCDN}/assets/js/ueditor/ueditor.all.js`);
+           //
+           // 百度编辑器的库地址
+           //
+           UIEditorControlProvider.setUrl(`${AdminCDN}/assets/js/ueditor/ueditor.config.js`, `${AdminCDN}/assets/js/ueditor/ueditor.all.js`);
 
-            //
-            // 上传空间的配置
-            //
-            UIUploadControlProvider.setDomain('七牛域名');
-            UIUploadControlProvider.setTokenUrl('七牛每次上传会调用这个URL, 返回算好的token, 然后才能上传');
-            UIUploadControlProvider.setMaxSize('1mb');
+           //
+           // 上传空间的配置
+           //
+           UIUploadControlProvider.setDomain('七牛域名');
+           UIUploadControlProvider.setTokenUrl('七牛每次上传会调用这个URL, 返回算好的token, 然后才能上传');
+           UIUploadControlProvider.setMaxSize('1mb');
 
-            //
-            // 表格配置项
-            //
-            UITableControlProvider.setRequestMethod('post');
-            UITableControlProvider.setResultName('aaData', 'iTotalRecords');
-            UITableControlProvider.setPageName('pageSize', 'pageNo');
-            UITableControlProvider.setConfig({});
+           //
+           // 表格配置项
+           //
+           UITableControlProvider.setRequestMethod('post');
+           UITableControlProvider.setResultName('aaData', 'iTotalRecords');
+           UITableControlProvider.setPageName('pageSize', 'pageNo');
+           UITableControlProvider.setConfig({});
 
-            //
-            // 树配置项
-            //
-            UITreeControlProvider.setDataName('id', 'name', 'pid');
-            UITreeControlProvider.setRequestMethod('post');
-        });
+           //
+           // 树配置项
+           //
+           UITreeControlProvider.setDataName('id', 'name', 'pid');
+           UITreeControlProvider.setRequestMethod('post');
+         });
 })();
--- src/service/data/ajax.js
+++ src/service/data/ajax.js
@@ -6,90 +6,95 @@
 //
 //-----------------------------------------------------------------------------------------------
 angular.module('admin.service')
-    .provider('Ajax', () => {
-        let successHandler,
-            failHandler,
-            result = {
-                setSuccessHandler(handler) {
-                    successHandler = handler;
-                },
+       .provider('Ajax', () => {
+         let successHandler,
+           failHandler,
+           host = '',
+           result = {
+             setSuccessHandler(handler) {
+               successHandler = handler;
+             },
 
-                setFailHandler(handler) {
-                    failHandler = handler;
-                },
+             setFailHandler(handler) {
+               failHandler = handler;
+             },
 
-                $get($q, Util, Message) {
-                    let _msg = new Message('Ajax'),
-                        _execute = (method, url, data) => {
-                            var defer = $q.defer();
-                            $.ajax({
-                                url: url, cache: false, data: data, type: method, dataType: 'json',
-                                success: (resData) => {
-                                    var success = successHandler(resData),
-                                        error = failHandler(resData);
-                                    if (success !== undefined && success !== null) {
-                                        defer.resolve(success);
-                                    }
-                                    else {
-                                        defer.reject(error);
-                                    }
-                                },
-                                error: (xhr, status) => {
-                                    var errMsg = {403: '没有权限', 404: '请求的地址不存在', 500: '服务器出现了问题,请稍后重试'}[status];
-                                    _msg.error(errMsg || '服务器出现了问题,请稍后重试');
-                                }
-                            });
-                            return defer.promise;
-                        };
-                    return {
+             setHost(h) {
+               host = h;
+             },
 
-                        get(url, data) {
-                            return _execute('GET', url, data);
-                        },
+             $get($q, Util, Message) {
+               let _msg = new Message('Ajax'),
+                 _execute = (method, url, data) => {
+                   var defer = $q.defer();
+                   $.ajax({
+                     url: host + url, cache: false, data: data, type: method, dataType: 'json',
+                     success: (resData) => {
+                       var success = successHandler(resData),
+                         error = failHandler(resData);
+                       if (success !== undefined && success !== null) {
+                         defer.resolve(success);
+                       }
+                       else {
+                         defer.reject(error);
+                       }
+                     },
+                     error: (xhr, status) => {
+                       var errMsg = {403: '没有权限', 404: '请求的地址不存在', 500: '服务器出现了问题,请稍后重试'}[status];
+                       _msg.error(errMsg || '服务器出现了问题,请稍后重试');
+                     }
+                   });
+                   return defer.promise;
+                 };
+               return {
 
-                        post(url, data) {
-                            return _execute('POST', url, data);
-                        },
+                 get(url, data) {
+                   return _execute('GET', url, data);
+                 },
 
-                        message(url, data, successMsg, failMsg) {
-                            return this.post(url, data)
-                                .then(() => _msg.success(successMsg))
-                                .catch(() => _msg.error(failMsg));
-                        },
+                 post(url, data) {
+                   return _execute('POST', url, data);
+                 },
 
-                        add(url, data) {
-                            return this.message(url, data, '添加数据成功', '添加数据失败');
-                        },
+                 message(url, data, successMsg, failMsg) {
+                   return this.post(url, data)
+                              .then(() => _msg.success(successMsg))
+                              .catch(() => _msg.error(failMsg));
+                 },
 
-                        update(url, data) {
-                            return this.message(url, data, '更新数据成功', '更新数据失败');
-                        },
+                 add(url, data) {
+                   return this.message(url, data, '添加数据成功', '添加数据失败');
+                 },
 
-                        remove(url, data, options) {
-                            options = options || {};
-                            return Util.confirm(`您确认删除该${options.label || '数据'}吗?`)
-                                .then(() => this[options.method ? options.method : 'get'](url, data, options));
-                        },
+                 update(url, data) {
+                   return this.message(url, data, '更新数据成功', '更新数据失败');
+                 },
 
-                        load(url) {
-                            var $dom = $('<div/>').hide().appendTo(document.body),
-                                defer = $q.defer();
-                            $dom.load(url, (html) => {
-                                $dom.remove();
-                                defer.resolve(html);
-                            });
-                            return defer.promise;
-                        },
+                 remove(url, data, options) {
+                   options = options || {};
+                   return Util.confirm(`您确认删除该${options.label || '数据'}吗?`)
+                              .then(() => this[options.method ? options.method : 'get'](url, data, options));
+                 },
 
-                        getScript(url) {
-                            return $.ajax({
-                                url: url,
-                                dataType: "script",
-                                cache: true
-                            });
-                        }
-                    };
-                }
-            };
-        return result;
-    });
+                 load(url) {
+                   var $dom = $('<div/>').hide().appendTo(document.body),
+                     defer = $q.defer();
+                   $dom.load(url, (html) => {
+                     $dom.remove();
+                     defer.resolve(html);
+                   });
+                   return defer.promise;
+                 },
+
+                 getScript(url) {
+                   return $.ajax({
+                     url: url,
+                     dataType: "script",
+                     cache: true
+                   });
+                 }
+               };
+             }
+           };
+         return result;
+       });
